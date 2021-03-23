3739734b8515dd62a8cc69720998406007c68f51
Patrick Muehlbauer <patrick@opentrash.org>
2021-03-24 00:43:58 +0100

    Add excoveralls for test coverage reports

--- mix.exs
+++ mix.exs
@@ -16,6 +16,13 @@ defmodule ArrowElixir.MixProject do
       docs: [
         main: "Arrow",
         extras: ["README.md"]
+      ],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
       ]
     ]
   end
@@ -33,7 +40,8 @@ defmodule ArrowElixir.MixProject do
       {:rustler, "~> 0.22-rc"},
       {:toml, "~> 0.5"},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
-      {:ex_parameterized, "~> 1.3.7", only: :test}
+      {:ex_parameterized, "~> 1.3.7", only: :test},
+      {:excoveralls, "~> 0.13.4", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
